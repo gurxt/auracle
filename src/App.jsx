c503fa0ac83effab3f83d011dcb69418f0e9ff7f
gurxt <jung_jungian@outlook.com>
2023-06-23 12:28:15 -0400

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stats } from '@react-three/drei'
 import './App.css'
 
-const App = () => {
-    return (
-        <div>
-
-        </div>
-    )
-} 
- 
-
-export default App
+export default function App() {
+  return (
+    <main className="app">
+      <Canvas shadows camera={{ position: [0, 1, 3] }}>
+        <OrbitControls
+          enableDamping={true} 
+          enablePan={false} 
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI / 2}
+        />
+        <gridHelper args={[10, 10, 0xff0000, 'teal']} />
+        <axesHelper args={[3]} />
+        <Stats />
+      </Canvas>
+    </main>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{js,jsx}"],
     theme: {
         extend: {},
     },
