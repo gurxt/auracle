2cad3a18bfa2b16f527217be77df5bf3a122b051
gurxt <jung_jungian@outlook.com>
2023-06-23 15:47:20 -0400

    le scene le forlamation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stats } from '@react-three/drei'
 import './App.css'
+import { useControls } from 'leva'
+import { Wednesday } from './components/draco/Wednesday'
+import { Gateways } from './components/draco/Gateways'
+import { Floor } from './components/draco/Floor'
 
-export default function App() {
+const App = () => {
+  const { show } = useControls('Helpers', {
+    show: true
+  })
+
+  const { x, y, z, intensity }= useControls('Camera', {
+    x: { value: -2, min: -10, max: 10, step: 0.1 },
+    y: { value: 2, min: -10, max: 10, step: 0.1 },
+    z: { value: -2, min: -10, max: 10, step: 0.1 },
+    intensity: { value: 1, min: -10, max: 10, step: 0.1 },
+    castShadow: true
+  })
+
   return (
     <main className="app">
-      <Canvas shadows camera={{ position: [0, 1, 3] }}>
-        <OrbitControls
-          enableDamping={true} 
-          enablePan={false} 
-          minPolarAngle={Math.PI / 6}
-          maxPolarAngle={Math.PI / 2}
-        />
-        <gridHelper args={[10, 10, 0xff0000, 'teal']} />
-        <axesHelper args={[3]} />
-        <Stats />
+      <Canvas camera={{ position: [0, 0, 3] }} shadows>
+        <pointLight
+          castShadow={true}
+          position={[x, y, z]}
+          intensity={intensity}
+        >
+          <mesh><sphereGeometry args={[0.25]} /></mesh>
+        </pointLight>
+        <Wednesday receiveShadow={true} />
+        <Gateways receiveShadow={true} />
+        <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI /2}/>
+        { show && (
+          <>
+          <gridHelper args={[20, 20, 0xff0000, 'teal']} />
+          <axesHelper args={[10]} />
+          <Stats />
+          </>
+        )}
+        <Floor scale={10} />
       </Canvas>
     </main>
   )
 }
+
+export default App
--- src/components/Wednesday-draco-transformed.jsx
+++
@@ -1,9 +0,0 @@
-const example = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default example
