a381ea1fb85c23c3bb5ff45d444f147d17765e4c
gurxt <jung_jungian@outlook.com>
2023-06-24 14:11:20 -0400

    le refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stats } from '@react-three/drei'
+import { Stats } from '@react-three/drei'
 import { useControls } from 'leva'
-import { Wednesday } from './components/draco/Wednesday'
-import { Terrain } from './components/draco/Terrain'
-import { Dome } from './components/draco/Dome'
-import { Doors } from './components/draco/Doors'
+import Scene1 from './components/Scene1'
 
 const App = () => {
   const { show } = useControls('Helpers', {
-    show: false
-  })
-
-  const { x, y, z, intensity } = useControls('Light', {
-    x: { value: -10, min: -10, max: 10, step: 0.1 },
-    y: { value: 4.7, min: -10, max: 10, step: 0.1 },
-    z: { value: 1.4, min: -10, max: 10, step: 0.1 },
-    intensity: { value: 1, min: -10, max: 10, step: 0.1 },
-    castShadow: true
+    show: true
   })
 
   return (
     <main className="app">
-      <Canvas camera={{ position: [-4, 2, 0] }} shadows>
-        <Wednesday />
-        <Doors position={[0, -1, 0]}/>
-        <Dome position={[0, -0.85, 0]}/>
-        <Terrain /> 
-        <directionalLight
-          castShadow={true}
-          position={[x, y, z]}
-          intensity={intensity}
-        >
-          <mesh><sphereGeometry args={[0.25]} /></mesh>
-        </directionalLight>
-        <OrbitControls
-          target={[0, 1, 0]}
-          enablePan={false}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-        />
+      <Canvas camera={{ position: [0, 0, 0] }} shadows>
+        <Scene1 />
         { show && (
           <>
-          <gridHelper position={[4, 0, 0]} args={[20, 10, 0xff0000, 'teal']} />
-          <axesHelper position={[4, 0, 0]} args={[10]} />
+          <gridHelper position={[0, 0, 0]} args={[20, 10, 0xff0000, 0xffffff ]} />
+          <axesHelper position={[0, 0, 0]} args={[10]} />
           <Stats />
           </>
         )}
---
+++ src/components/Scene1.jsx
@@ -0,0 +1,40 @@
+import { Environment, OrbitControls } from "@react-three/drei"
+import { useControls } from "leva"
+import { Model } from "./draco/Scene1"
+
+const Scene1 = () => {
+  const { x, y, z, intensity } = useControls('Light', {
+    x: { value: 0.6, min: -10, max: 10, step: 0.1 },
+    y: { value: -3.7, min: -10, max: 10, step: 0.1 },
+    z: { value: 6.8, min: -10, max: 10, step: 0.1 },
+    intensity: { value: 1, min: -10, max: 10, step: 0.1 },
+    castShadow: true
+  })
+  
+  return (
+    <>
+    <Environment
+      preset="night"
+      background
+    />
+    <Model />
+    <directionalLight
+      castShadow={true}
+      position={[x, y, z]}
+      intensity={intensity}
+    >
+      <mesh><sphereGeometry args={[0.25]} /></mesh>
+    </directionalLight>
+    <OrbitControls
+      target={[0, 1, 0]}
+      enablePan={false}
+      enableZoom={false}
+      minAzimuthAngle={-Math.PI / 2 }
+      maxAzimuthAngle={Math.PI / 2 }
+      maxPolarAngle={Math.PI / 2}
+    />
+    </>
+  )
+}
+
+export default Scene1
