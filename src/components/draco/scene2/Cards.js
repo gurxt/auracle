518b6c6b95d8a0b10a132de46e6be8c210844e0d
gurxt <jung_jungian@outlook.com>
2023-07-10 14:09:33 -0400

    changing import

--- src/components/draco/scene2/Cards.js
+++ src/components/draco/scene2/Cards.js
@@ -1,11 +1,12 @@
-import zero from '/src/assets/tarots/0_TheFool.jpg'
-import one from '/src/assets/tarots/1_TheMagician.jpg'
-import two from '/src/assets/tarots/2_TheHighPriestess.jpg'
-import three from '/src/assets/tarots/3_TheEmpress.jpg'
-import four from '/src/assets/tarots/4_TheEmperor.jpg'
-import five from '/src/assets/tarots/5_TheHierophant.jpg'
-import six from '/src/assets/tarots/6_TheLovers.jpg'
-import seven from '/src/assets/tarots/7_TheChariot.jpg'
+import zero from './src/assets/tarots/0_TheFool.jpg'
+import one from './src/assets/tarots/1_TheMagician.jpg'
+import two from './src/assets/tarots/2_TheHighPriestess.jpg'
+import three from './src/assets/tarots/3_TheEmpress.jpg'
+import four from './src/assets/tarots/4_TheEmperor.jpg'
+import five from './src/assets/tarots/5_TheHierophant.jpg'
+import six from './src/assets/tarots/6_TheLovers.jpg'
+import seven from './src/assets/tarots/7_TheChariot.jpg'
+/*
 import eight from '/src/assets/tarots/8_Strength.jpg'
 import nine from '/src/assets/tarots/9_TheHermit.jpg'
 import ten from '/src/assets/tarots/10_WheelOfFortune.jpg'
@@ -20,6 +21,7 @@ import eighteen from '/src/assets/tarots/18_TheMoon.jpg'
 import nineteen from '/src/assets/tarots/19_TheSun.jpg'
 import twenty from '/src/assets/tarots/20_Judgement.jpg'
 import twentyOne from '/src/assets/tarots/21_TheWorld.jpg'
+*/
 
 const cards = [
   zero,
