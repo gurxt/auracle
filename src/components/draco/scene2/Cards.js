44cbb93250cedc01bf0ec40827f837e41dafae65
gurxt <jung_jungian@outlook.com>
2023-07-10 14:14:06 -0400

    images oh images

--- src/components/draco/scene2/Cards.js
+++ src/components/draco/scene2/Cards.js
@@ -1,53 +1,26 @@
-import zero from './src/assets/tarots/0_TheFool.jpg'
-import one from './src/assets/tarots/1_TheMagician.jpg'
-import two from './src/assets/tarots/2_TheHighPriestess.jpg'
-import three from './src/assets/tarots/3_TheEmpress.jpg'
-import four from './src/assets/tarots/4_TheEmperor.jpg'
-import five from './src/assets/tarots/5_TheHierophant.jpg'
-import six from './src/assets/tarots/6_TheLovers.jpg'
-import seven from './src/assets/tarots/7_TheChariot.jpg'
-/*
-import eight from '/src/assets/tarots/8_Strength.jpg'
-import nine from '/src/assets/tarots/9_TheHermit.jpg'
-import ten from '/src/assets/tarots/10_WheelOfFortune.jpg'
-import eleven from '/src/assets/tarots/11_Justice.jpg'
-import twelve from '/src/assets/tarots/12_TheHangedMan.jpg'
-import thirteen from '/src/assets/tarots/13_Death.jpg'
-import fourteen from '/src/assets/tarots/14_Temperance.jpg'
-import fifteen from '/src/assets/tarots/15_TheDevil.jpg'
-import sixteen from '/src/assets/tarots/16_TheTower.jpg'
-import seventeen from '/src/assets/tarots/17_TheStar.jpg'
-import eighteen from '/src/assets/tarots/18_TheMoon.jpg'
-import nineteen from '/src/assets/tarots/19_TheSun.jpg'
-import twenty from '/src/assets/tarots/20_Judgement.jpg'
-import twentyOne from '/src/assets/tarots/21_TheWorld.jpg'
-*/
-
 const cards = [
-  zero,
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine,
-  ten,
-  eleven,
-  twelve,
-  thirteen,
-  fourteen,
-  fifteen,
-  sixteen,
-  seventeen,
-  eighteen,
-  nineteen,
-  twenty,
-  twentyOne
+  '0_TheFool.jpg',
+  '1_TheMagician.jpg',
+  '2_TheHighPriestess.jpg',
+  '3_TheEmpress.jpg',
+  '4_TheEmperor.jpg',
+  '5_TheHierophant.jpg',
+  '6_TheLovers.jpg',
+  '7_TheChariot.jpg',
+  '8_Strength.jpg',
+  '9_TheHermit.jpg',
+  '10_WheelOfFortune.jpg',
+  '11_Justice.jpg',
+  '12_TheHangedMan.jpg',
+  '13_Death.jpg',
+  '14_Temperance.jpg',
+  '15_TheDevil.jpg',
+  '16_TheTower.jpg',
+  '17_TheStar.jpg',
+  '18_TheMoon.jpg',
+  '19_TheSun.jpg',
+  '20_Judgement.jpg',
+  '21_TheWorld.jpg'
 ]
-
-
 
 export default cards
